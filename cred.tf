c9363ee2c556c3a35856f40b8587886b4c4ef356
bernardo jose <bernardo.jose@websummit.com>
2023-05-23 07:18:16 +0100

    fixed code format

--- cred.tf
+++ cred.tf
@@ -1,15 +1,15 @@
 variable "access_key" {
-  default = "AKIA2IBQ5UZBR7IIXNHD"
+  default   = "AKIA2IBQ5UZBR7IIXNHD"
   sensitive = true
-  }
+}
 
 variable "secret_key" {
-  default = "hbBEUmWmOq8JY3mYgEJIFAGUnsaZbUsuOK4vSSXe"
+  default   = "hbBEUmWmOq8JY3mYgEJIFAGUnsaZbUsuOK4vSSXe"
   sensitive = true
-  }
+}
 
-  variable "region" {
+variable "region" {
   description = "AWS Region"
   type        = string
-  default = "eu-west-1"
+  default     = "eu-west-1"
 }
