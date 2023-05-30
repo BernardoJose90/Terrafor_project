1931cb29d4b5cda1d9ee97614b98e5f6defe20a0
bernardo jose <bernardo.jose@websummit.com>
2023-05-30 21:50:50 +0100

    restructure project

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,4 +1,7 @@
 
-module "vpc1" {
-  source = "../modules/vpc_eu-west-1"
+module "create_pvc" {
+  source = "../modules/vpc"
+  cidr_block = "10.0.0.0/16 "
+  public_subnets = "10.0.90.0/24"
+  private_subnets = "10.0.100.0/24"
 }
