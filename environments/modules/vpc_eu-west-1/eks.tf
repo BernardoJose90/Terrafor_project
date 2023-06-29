62210bc6ad786a287451ab36e68572fa7013dba0
bernardo jose <bernardo.jose@websummit.com>
2023-06-29 11:28:13 -0400

    added EKS service

---
+++ environments/modules/vpc_eu-west-1/eks.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = "us-west-2"  # Replace with your desired AWS region
+}
+
+resource "aws_eks_cluster" "example" {
+  name     = "my-cluster"
+  role_arn = aws_iam_role.cluster.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.public.id   # Replace with your subnet IDs
+  }
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_iam_role" "cluster" {
+  name = "my-cluster-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_amazon_eks_admin" {
+  role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_amazon_ec2_container_registry_read_only" {
+  role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+output "kubeconfig" {
+  value = aws_eks_cluster.example.kubeconfig
+}
